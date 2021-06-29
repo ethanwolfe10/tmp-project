cfd6b164e7ccf2c0690b74bf9e05b6239409cfda
ethanwolfe10 <ethanwolfe@comcast.net>
2021-06-29 15:06:30 -0700

    full project commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,6 +25,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  require "omniauth-facebook"
   config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET']
 
   # Configure the class responsible to send e-mails.
