4de2357f106cced1786be8f2bb5c5d225f05bf07
ethanwolfe10 <ethanwolfe@comcast.net>
2021-06-29 21:57:11 -0700

    Follows index action now renders follows/index and appropriately assigns either followings or followers variable

--- app/helpers/follows_helper.rb
+++ app/helpers/follows_helper.rb
@@ -1,2 +1,11 @@
 module FollowsHelper
+
+    def not_following?(user)
+        if !Follow.find_by(follower_id: current_user.id, followed_user_id: user.id)
+            return true
+        else
+            Follow.find_by(follower_id: current_user.id, followed_user_id: user.id)
+        end
+    end
+
 end
