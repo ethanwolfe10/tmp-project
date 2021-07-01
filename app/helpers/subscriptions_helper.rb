e5f443add6be8a0e2babf4485f67d476a50f0228
ethanwolfe10 <ethanwolfe@comcast.net>
2021-06-30 23:31:16 -0700

    new subscription page renders no members available if none of current_users followers can be invited to the group.

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -3,4 +3,15 @@ module SubscriptionsHelper
     def sub_to_group(sub)
         Group.find_by(id: sub.group_id)
     end
+
+    def subscription_exist(user)
+        return true if Subscription.find_by(user_id: user.id, group_id: params[:group_id])
+    end
+
+    def no_eligible_members?
+        current_user.followers.each do |fllwr|
+            return true if Subscription.find_by(user_id: fllwr.id, group_id: params[:group_id]) == nil
+        end
+    end
+
 end
