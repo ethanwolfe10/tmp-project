216071bc430fd602e2e3792b312205e0b2c86685
ethanwolfe10 <ethanwolfe@comcast.net>
2021-07-01 00:26:00 -0700

    confirmed subscribers now have the option to leave the group. added a after_delete action that check to see if a group is empty after one of its subscribers leave, and will delete itself if true. no more ghost groups

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,5 +1,8 @@
 class SubscriptionsController < ApplicationController
 
+    after_action :last_subscription, only: :destroy
+    
+
     def new
         @subscription = Subscription.new
     end
@@ -9,10 +12,25 @@ class SubscriptionsController < ApplicationController
         if @new_sub.save
             redirect_to new_group_subscription_path
         end
+    end
 
+    def destroy        
+        @sub = Subscription.find(params[:id])
+        if @sub.user_id == current_user.id
+            @sub.destroy
+            redirect_to user_path(current_user)
+        end
     end
 
     private
+
+    def last_subscription
+        if Group.find(params[:id]).subscribers.empty?
+            Group.find(params[:id]).destroy
+        else
+            return true
+        end
+    end
 
     def subscription_params
         params.require(:subscription).permit(:user_id, :group_id, :moderator, :confirmed)
