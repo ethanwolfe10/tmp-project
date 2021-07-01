a9f310152c73fdee00bfd8d748269dae1a3ba210
ethanwolfe10 <ethanwolfe@comcast.net>
2021-07-01 12:56:48 -0700

    bug where helper method that would determine where a user is already following another before displaying, would access a blank current_user

--- app/helpers/follows_helper.rb
+++ app/helpers/follows_helper.rb
@@ -1,11 +1,7 @@
 module FollowsHelper
 
-    def not_following?(user)
-        if !Follow.find_by(follower_id: current_user.id, followed_user_id: user.id)
-            return true
-        else
-            Follow.find_by(follower_id: current_user.id, followed_user_id: user.id)
-        end
+    def follow_record(user)
+        Follow.find_by(follower_id: current_user.id, followed_user_id: user.id)
     end
 
 end
