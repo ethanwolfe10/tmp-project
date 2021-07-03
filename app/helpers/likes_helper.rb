1c1389e18d40fd0c1006b0582e9ec95020bd97a0
ethanwolfe10 <ethanwolfe@comcast.net>
2021-07-03 00:29:41 -0700

    bug with displaying errors. cannot render a partial for errors.

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,7 @@
 module LikesHelper
+
+    def return_like(post, user)
+        Like.find_by(post_id: post.id, user_id: user.id)
+    end
+    
 end
--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,10 +1,11 @@
 module SubscriptionsHelper
 
-    def sub_to_group(sub)
-        Group.find_by(id: sub.group_id)
-    end
+    # def unconfirmed_subs(subs)
+    #     binding.pry
+    #     subs.where(confirmed: false)
+    # end
 
-    def subscription_exist(user)
+    def subscription_exists(user)
         return true if Subscription.find_by(user_id: user.id, group_id: params[:group_id])
     end
 
@@ -12,6 +13,10 @@ module SubscriptionsHelper
         current_user.followers.each do |fllwr|
             return true if Subscription.find_by(user_id: fllwr.id, group_id: params[:group_id]) == nil
         end
+    end
+
+    def current_sub(group)
+        Subscription.find_by(user_id: current_user.id, group_id: group.id)
     end
 
 end
